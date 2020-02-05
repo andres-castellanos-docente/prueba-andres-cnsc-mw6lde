d3a683cc93225c0486daff817114f5ee5a1d869f
andresrobanesky <andresrobanesky@gmail.com>
2020-02-04 21:34:43 -0500

    se añade español

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit  {
   name = "Angular";
+  constructor(
+      private translate: TranslateService) {
+  }
+
+ngOnInit() {
+  const def = 'es';
+  // Verifica el lenguaje del navegador para setear idioma.
+  const getNavigatorLanguage: string = (navigator.languages && navigator.languages.length) ?
+      navigator.languages[0] : navigator.language || def;
+  let index = getNavigatorLanguage.indexOf('-');
+  if (index === -1) {
+    index = getNavigatorLanguage.length;
+  }
+  let lang = getNavigatorLanguage.substring(0, index);
+  // verifica si no cumple ninguna de las dos setea por default.
+  if ((lang !== 'es') && (lang !== 'en')) {
+    lang = def;
+  }
+  lang = 'es';
+  this.translate.setDefaultLang(lang);
+}
 }
